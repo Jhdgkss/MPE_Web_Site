9320af58916f0917ec66220d7d46987b84be1aed
John <youremail@example.com>
2026-03-10 11:31:04 +0000

    Updated URLS.py for favicon.png

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.templatetags.static import static as static_file
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Fix for favicon requests (Google / browsers look for /favicon.ico)
+    path("favicon.ico", RedirectView.as_view(url=static_file("assets/favicon.png"))),
+
+    # Main site routes
     path("", include("core.urls")),
 ]
 
